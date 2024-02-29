a0b62a6ece3ec07fff15aa993333b9853fbaa61e
Qmanh <quanmanh567@gmail.com>
2024-02-29 11:20:05 +0700

    update: check method

--- api/views.py
+++ api/views.py
@@ -21,12 +21,21 @@ def ApiOverview(request):
 @api_view(['POST'])
 def predict_url(request):
     item = PredictionSerializer(data=request.data)
-
+    print(item)
     if item.is_valid():
         # item.save()
-        domain = item.data.get('domain')
+        domain = check_domain_or_url(item.data.get('domain'))
         predict_model = model.get_model()
         result = Phishing_detection.predict(predict_model, domain)
         return Response({"data": result}, status=status.HTTP_201_CREATED)
     else:
         return Response(item.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def check_domain_or_url(input_string):
+    url_regex = r"^(https?|ftp)://[^\s/$.?#].[^\s]*$"
+
+    if re.match(url_regex, input_string):
+        return input_string
+    else:
+       return "https://" + input_string
